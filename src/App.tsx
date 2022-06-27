6a2791f574308aee00403c3d10c77191c9880b17
felipeghidini <lipeghidini18@gmail.com>
2022-06-27 13:00:17 -0300

    Roteamento e player da aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Header } from "./components/Header"
-import { Lesson } from "./components/Lesson"
-import { Sidebar } from "./components/Sidebar"
-import { Video } from "./components/Video"
-import { Event } from "./pages/Event"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
 
 
 
 function App() {
-  
-  
   return (
-    <div>
-      <Event />
-    </div>
-    )
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+
+  )
 }
 
 export default App
